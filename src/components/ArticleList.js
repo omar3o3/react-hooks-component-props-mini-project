0a33672367d6e841e156505143d0b79e4c01691c
omar3o3 <otantawy53@gmail.com>
2022-05-18 11:58:02 -0400

    commit tree e4a899411e63be639ea864d017f38b26acf9e719

---
+++ src/components/ArticleList.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Article from './Article'
+
+function ArticleList({posts}) {
+
+  const articles = (posts.map(article => {
+      console.log(article);
+    return (
+        <Article
+        key = {article.id}
+        title = {article.title}
+        date = {article.date}
+        preview = {article.preview}
+        />
+    )
+  }))
+
+  return (
+    <main>
+        {articles}
+    </main>
+  )
+}
+
+export default ArticleList
